6e8b623512ec6087ff8791d2a75f8b9612169f72
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2024-07-08 10:04:39 +0100

    another commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,15 +7,23 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sanity({
-    projectId: "7nolqz71",
-    dataset: "production",
-    useCdn: true,
-    studioBasePath: "/admin"
-  })],
+  integrations: [
+    tailwind(),
+    react(),
+    sanity({
+      projectId: "7nolqz71",
+      dataset: "production",
+      useCdn: true,
+      studioBasePath: "/admin",
+    }),
+  ],
   output: "server",
   devToolbar: {
-    enabled: false
+    enabled: false,
   },
-  adapter: netlify()
+  adapter: netlify(),
+
+  image: {
+    remotePatterns: [{ protocol: "http" }, { protocol: "https" }],
+  },
 });
